13f75eebecfe5289381c80e9033e495e208df161
Adarsh Desai <desaiadarsh22@gmail.com>
2022-12-24 15:31:58 +0530

    Selecting elements in DOm and added reset when page loads

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Selecting elements
+
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+const btnHoldElement = document.querySelector('.btn--hold');
+const btnNewElement = document.querySelector('.btn--new');
+const btnRollElement = document.querySelector('.btn--roll');
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Hide the dice on page load
+  const diceElement = document.querySelector('.dice');
+  diceElement.classList.add('hidden');
+  // Reset scores to 0 on page load
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+});
